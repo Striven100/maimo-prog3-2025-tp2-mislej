a08cfcead29aae2be0b3a6b46bb8ce25d95d7669
Tobias Mislej <tobimislej@gmail.com>
2025-06-12 20:31:14 -0300

    Añadi MovieContainer

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import Link from "next/link";
 
 const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500'
 
-export default function MoviesGrid({ movies }) {
+export default function MoviesGrid({ movies, id }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
       {movies.map(movie => (
@@ -17,6 +18,7 @@ export default function MoviesGrid({ movies }) {
           <div className="p-4">
             <h3 className="text-lg font-semibold text-white truncate">{movie.title}</h3>
             <p className="text-sm text-gray-400">{movie.release_date}</p>
+            <Link href={`movie/${id}`} className="inline-block mt-6 px-6 py-2 bg-yellow-500 rounded-lg font-medium text-black hover:bg-yellow-600 transition-colors">Ver más</Link>
           </div>
         </div>
       ))}
